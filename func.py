8e00f7105c057b56a31911ecd6cfed18757f6ff8
aoiupen <aoiudesign@gmail.com>
2022-08-15 00:28:43 +0900

    add realtion with start path, target path

--- func.py
+++ func.py
@@ -1,4 +1,5 @@
 import os
+from typing import Sequence
 from PyQt5.QtWidgets import QFileDialog
 import main
 
@@ -6,16 +7,30 @@ import main
 def get_target_path(window, ui):
     path = str(QFileDialog.getExistingDirectory(window, "Select Directory"))
     ui.label_target_path.setText(path)
-    if ui.label_target_path.text() and ui.label_root_path.text():
-        get_rel_path(ui, ui.label_target_path.text(), ui.label_root_path.text())
+    if ui.label_target_path.text() and ui.label_start_path.text():
+        get_rel_path(ui, ui.label_target_path.text(), ui.label_start_path.text())
 
-def get_root_path(window, ui):
-    path = str(QFileDialog.getExistingDirectory(window, "Select Directory"))
-    ui.label_root_path.setText(path)
-    if ui.label_target_path.text() and ui.label_root_path.text():
-        get_rel_path(ui, ui.label_target_path.text(), ui.label_root_path.text())
+def get_start_path(window, ui):
+    path = str(QFileDialog.getExistingDirectory(window, "Select Directory"))    
+    ui.label_start_path.setText(path)
+    if ui.label_target_path.text() and ui.label_start_path.text():
+        get_rel_path(ui, ui.label_target_path.text(), ui.label_start_path.text())
 
-def get_rel_path(ui, label_target_path, label_root_path):
-    path = os.path.relpath(label_target_path, label_root_path)
+def get_rel_path(ui, label_target_path, label_start_path):
+    path = os.path.relpath(label_target_path, label_start_path)
     ui.label_rel_path.setText(path)
-    pass
+    tup_each_path = get_com_path(ui, label_target_path, label_start_path)
+    
+
+def get_com_path(ui, path1,path2):
+    #dialog : \\ (window)
+    #os.path : /
+    compath = os.path.commonpath([path1,path2])
+    compath = compath.replace("\\","/") #to os.path style
+    remain_path1 = path1.lstrip(compath)
+    remain_path2 = path2.lstrip(compath)
+    
+    ui.label_com_path.setText(compath)
+    ui.label_each1_path.setText(remain_path1)
+    ui.label_each2_path.setText(remain_path2)
+    return remain_path1, remain_path2
--- ui.py
+++ ui.py
@@ -1,6 +1,8 @@
-from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QLabel, QLineEdit, QCheckBox, QPushButton, QFileDialog
-from PyQt5.QtCore import Qt, QRect
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QLabel, QLineEdit, QCheckBox, QPushButton, QFileDialog, QSizePolicy
+from PyQt5.QtCore import Qt, QRect,QSize
+import os
 import func
+
 
 
 class MainUi():
@@ -10,38 +12,38 @@ class MainUi():
     def setup_ui(self, MainWindow, app):
         self.app = app
         self.window = MainWindow
-        self.window.showMinimized()
         self.window.setWindowTitle("rel path")
         self.ctr_widget = QWidget()
         self.window.setCentralWidget(self.ctr_widget)
         self.ctr_layout = QVBoxLayout(self.ctr_widget)
+        self.ctr_layout.setAlignment(Qt.AlignTop)
 
         self.layout_rel = QHBoxLayout()
         self.layout_target = QHBoxLayout()
-        self.layout_root = QHBoxLayout()
+        self.layout_start = QHBoxLayout()
         
         self.label_target = QLabel("Target path : ")
-        self.label_root = QLabel("Root path : ")
+        self.label_start = QLabel("Start path : ")
         self.label_target.setFixedWidth(100)
-        self.label_root.setFixedWidth(100)
+        self.label_start.setFixedWidth(100)
         self.label_target_path = QLabel()
-        self.label_root_path = QLabel()
+        self.label_start_path = QLabel()
 
         self.btn_target_path_dialog = QPushButton("...")
-        self.btn_root_path_dialog = QPushButton("...")
+        self.btn_start_path_dialog = QPushButton("...")
         self.btn_target_path_dialog.setFixedWidth(30)
-        self.btn_root_path_dialog.setFixedWidth(30)
+        self.btn_start_path_dialog.setFixedWidth(30)
         self.btn_target_path_dialog.clicked.connect(
             lambda: func.get_target_path(self.window, self))
-        self.btn_root_path_dialog.clicked.connect(
-            lambda: func.get_root_path(self.window, self))
+        self.btn_start_path_dialog.clicked.connect(
+            lambda: func.get_start_path(self.window, self))
 
         self.layout_target.addWidget(self.label_target)
         self.layout_target.addWidget(self.label_target_path)
         self.layout_target.addWidget(self.btn_target_path_dialog)
-        self.layout_root.addWidget(self.label_root)
-        self.layout_root.addWidget(self.label_root_path)
-        self.layout_root.addWidget(self.btn_root_path_dialog)
+        self.layout_start.addWidget(self.label_start)
+        self.layout_start.addWidget(self.label_start_path)
+        self.layout_start.addWidget(self.btn_start_path_dialog)
         
         #
         self.label_rel = QLabel("Relative path : ")
@@ -50,5 +52,53 @@ class MainUi():
         self.layout_rel.addWidget(self.label_rel) 
         self.layout_rel.addWidget(self.label_rel_path) 
         self.ctr_layout.addLayout(self.layout_rel)
-        self.ctr_layout.addLayout(self.layout_root)
+        self.ctr_layout.addLayout(self.layout_start)
         self.ctr_layout.addLayout(self.layout_target)
+        
+        #
+        self.btn = QPushButton("btn")
+        self.hidden = True
+        self.btn.clicked.connect(self.hide)
+        self.ctr_layout.addWidget(self.btn)
+        self.lineedit = QLineEdit()
+        self.lineedit.hide()
+        self.ctr_layout.addWidget(self.lineedit)
+
+        self.layout_com_path = QHBoxLayout()   
+        self.layout_each_path1 =QHBoxLayout()   
+        self.layout_each_path2 = QHBoxLayout() 
+        
+        self.label_com = QLabel("Common Path : ")
+        self.label_each1 = QLabel("Start Path : ")
+        self.label_each2 = QLabel("Target Path : ")
+        
+        self.label_com_path = QLabel()
+        self.label_each1_path = QLabel()
+        self.label_each2_path = QLabel()
+        
+        self.layout_com_path.addWidget(self.label_com)
+        self.layout_com_path.addWidget(self.label_com_path)
+        self.layout_each_path1.addWidget(self.label_each1)
+        self.layout_each_path1.addWidget(self.label_each1_path)
+        self.layout_each_path2.addWidget(self.label_each2)
+        self.layout_each_path2.addWidget(self.label_each2_path)
+        
+        self.ctr_layout.addLayout(self.layout_com_path)
+        self.ctr_layout.addLayout(self.layout_each_path1)
+        self.ctr_layout.addLayout(self.layout_each_path2)
+        
+        
+
+        
+    def hide(self):
+        if self.hidden:
+            print(1)
+            self.hidden = False
+            self.lineedit.show()
+            #self.label_start_path.setText('<font color="blue">{a}</font><font color="red"> World</font><font color="green">!</font>'.format(a="1"))
+
+        else:
+            self.lineedit.hide()
+            self.hidden = True
+            
+    
